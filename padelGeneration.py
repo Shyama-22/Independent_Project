292e5235986f206ac9f4de3eed77312898ba2925
vinit21501 <103728474+vinit21501@users.noreply.github.com>
2024-02-26 11:21:53 +0530

    padel features generation code
    Signed-off-by: vinit21501 <103728474+vinit21501@users.noreply.github.com>

---
+++ padelGeneration.py
@@ -0,0 +1,28 @@
+import padelpy
+import pandas as pd
+from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
+
+toxinData = pd.read_excel('padelFinds.xlsx')
+
+def generate(arg):
+    try:
+        feat = padelpy.from_smiles(arg[0], fingerprints=True)
+        feat['pubchem id'] = arg[1]
+        feat['smile'] = arg[0]
+        return feat
+    except:
+        print('except', arg)
+        return None
+
+data = toxinData
+feat = padelpy.from_smiles(data['smile'][0], fingerprints=True)
+feat['pubchem id'] = data['pubchem id'][0]
+feat['smile'] = data['smile'][0]
+padelFeature = pd.DataFrame([feat])
+with ThreadPoolExecutor(max_workers = 4) as executor:
+    results = executor.map(generate, zip(data['smile'][1:], data['pubchem id'][1:]))
+    for result in tqdm (results, desc='extracting'):
+        if result:
+            padelFeature.loc[len(padelFeature)] = result
+padelFeature.to_excel('features.xlsx', index=False)
