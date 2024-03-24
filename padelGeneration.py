aa87ad108f23405fd832295f03d243d0cdd31414
vinit21501 <vinit21501@iiitd.ac.in>
2024-03-24 12:54:09 +0530

    toxic features replace

--- padelGeneration.py
+++ padelGeneration.py
@@ -4,7 +4,9 @@ from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor
 
 # toxinData = pd.read_excel('padelFinds.xlsx')
-nontoxinData = pd.read_excel('allnontoxicsmiles.xlsx')
+nontoxinData = pd.read_excel('toxinSmile.xlsx')
+nontoxinData = nontoxinData.drop_duplicates(subset=['pubchem id'])
+nontoxinData.rename(columns={'pubchem id' : 'pcid'}, inplace=True)
 # nontoxinData.rename(columns={'pubchemCID':'pubchemid'}, inplace=True)
 
 def generate(arg):
@@ -18,13 +20,13 @@ def generate(arg):
         return None
 
 data = nontoxinData
-feat = padelpy.from_smiles(data['smile'][0], fingerprints=True)
-feat['pcid'] = data['pcid'][0]
-feat['smile'] = data['smile'][0]
+feat = padelpy.from_smiles(data['smile'][4], fingerprints=True, timeout=60)
+feat['pcid'] = data['pcid'][4]
+feat['smile'] = data['smile'][4]
 padelFeature = pd.DataFrame([feat])
 with ThreadPoolExecutor(max_workers = 4) as executor:
-    results = executor.map(generate, zip(data['smile'][1:], data['pcid'][1:]))
+    results = executor.map(generate, zip(data['smile'], data['pcid']))
     for result in tqdm (results, desc='extracting'):
         if result:
             padelFeature.loc[len(padelFeature)] = result
-padelFeature.to_excel('nontoxicpadelfeatures.xlsx', index=False)
+padelFeature.to_excel('alloxicpadelfeatures.xlsx', index=False)
